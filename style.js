b07c45f83b2fc66612006047e18c9823f594914f
Maite <anamaitegarciacorrea@gmail.com>
2024-12-18 19:07:30 +0100

    Añado animación del árbol

--- style.js
+++ style.js
@@ -11,11 +11,29 @@ buttons[0].addEventListener("click",()=>{
 
 buttons[1].addEventListener("click",()=>{
     contenedorBotones.classList.add("show");  // Añade la clase para activar la transición
-    mensajeContainer.style.display="none";
+    // mensajeContainer.style.display="none";
+    mensajeContainer.textContent="";
     mensajeSlogan.style.display="none";
+    mensajeContainer.classList.add("arbol");
+    mensajeContainer.innerHTML=`
+        <div id="christmas-tree" class="christmas-tree">
+        <div class="tree">
+          <div class="tree-details"></div>
+        </div>
+        <div class="lights">
+          <div class="row-one"></div>
+          <div class="row-two"></div>
+        </div>
+        <div class="balls"></div>
+        <div class="star"></div>
+        <div class="shadow"></div>
+      </div>
+    `;
 })
 
 
 
 //CURSOR
 document.body.style.cursor = "url('./img/christmas_tree_PNG12.png'), auto";
+
+
