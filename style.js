329b10a6373d01d4314616ee453bc05c95645ae0
Maite <anamaitegarciacorrea@gmail.com>
2024-12-18 18:22:30 +0100

    Antes de añadir el árbol

--- style.js
+++ style.js
@@ -1,8 +1,21 @@
 const buttons=document.querySelectorAll("button");
 const feliz=document.querySelector(".feliz");
+const mensajeContainer=document.querySelector(".mensaje");
+const mensajeSlogan=document.querySelector(".slogan");
+const contenedorBotones=document.querySelector(".container")
 console.log(buttons);
 
 buttons[0].addEventListener("click",()=>{
     feliz.textContent="MERRY CHRISTMAS!";
-    // feliz.style.color="green";
 })
+
+buttons[1].addEventListener("click",()=>{
+    contenedorBotones.classList.add("show");  // Añade la clase para activar la transición
+    mensajeContainer.style.display="none";
+    mensajeSlogan.style.display="none";
+})
+
+
+
+//CURSOR
+document.body.style.cursor = "url('./img/christmas_tree_PNG12.png'), auto";
